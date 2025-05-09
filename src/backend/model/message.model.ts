1789e6cffb365c90d3a9657b5732c12558bee8f7
Aman Kumar <amankarguwal0@gmail.com>
2025-05-09 07:58:20 +0530

    Add Message model with schema definition for message data

---
+++ src/backend/model/message.model.ts
@@ -0,0 +1,51 @@
+import mongoose, { Document, Schema, Types } from "mongoose";
+
+export interface Message extends Document {
+  sender: Types.ObjectId;
+  chat: Types.ObjectId;
+  content: string;
+  attachments: {
+    public_id: string;
+    url: string;
+  }[];
+}
+
+const MessageSchema: Schema<Message> = new Schema(
+  {
+    sender: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "Sender ID is required"],
+    },
+    chat: {
+      type: Schema.Types.ObjectId,
+      ref: "Chat",
+      required: [true, "Chat ID is required"],
+    },
+    content: {
+      type: String,
+      trim: true,
+    },
+    attachments: [
+      {
+        public_id: {
+          type: String,
+          required: [true, "Public ID is required"],
+        },
+        url: {
+          type: String,
+          required: [true, "URL is required"],
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const MessageModel =
+  (mongoose.models.Message as mongoose.Model<Message>) ||
+  mongoose.model<Message>("Message", MessageSchema);
+
+export default MessageModel;
