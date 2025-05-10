e60260acbac00a965b46a61aff94cadcf40559c2
Aman Kumar <amankarguwal0@gmail.com>
2025-05-10 17:06:12 +0530

    Remove unused API integration from store configuration

--- src/lib/store/store.ts
+++ src/lib/store/store.ts
@@ -2,7 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./auth.reducer";
 import miscSlice from "./misc.reducer";
 import chatSlice from "./chat.reducer";
-import api from "./api";
 
 export const makeStore = () => {
   return configureStore({
@@ -10,10 +9,7 @@ export const makeStore = () => {
       [authSlice.name]: authSlice.reducer,
       [miscSlice.name]: miscSlice.reducer,
       [chatSlice.name]: chatSlice.reducer,
-      [api.reducerPath]: api.reducer,
     },
-    middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware().concat(api.middleware),
   });
 };
 
