1c3c09912bf750e348b3cdcca6c877eaa1e79545
Aman Kumar <amankarguwal0@gmail.com>
2025-05-10 17:05:38 +0530

    Refactor hooks: update error type definition in useErrors and add useGetOrSaveFromStorage for local storage management

--- src/hooks/hook.tsx
+++ src/hooks/hook.tsx
@@ -1,10 +1,11 @@
-import { useEffect } from "react";
+import { NewMessageAlert } from "@/lib/store/chat.reducer";
+import { useEffect, useState } from "react";
 import { useToast } from "./use-toast";
 
 const useErrors = (
   errors: {
     isError: boolean;
-    error: any;
+    error: string | null;
   }[]
 ) => {
   const { toast } = useToast();
@@ -35,4 +36,33 @@ const useErrors = (
   }, [errors, toast]);
 };
 
-export default useErrors;
+
+type StorageOptions = {
+  key: string;
+  value?: NewMessageAlert[];
+  fallbackValue?: NewMessageAlert[];
+};
+
+const useGetOrSaveFromStorage = ({
+  key,
+  value,
+  fallbackValue = [],
+}: StorageOptions) => {
+  const [item, setItem] = useState(fallbackValue);
+  useEffect(() => {
+    const stored = localStorage.getItem(key);
+    setItem(stored ? JSON.parse(stored) : fallbackValue);
+  }, [fallbackValue, key]);
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [item, value]);
+
+  return {
+    item,
+    setItem,
+  }
+};
+
+export { useErrors, useGetOrSaveFromStorage };
+
