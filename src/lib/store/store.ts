384d2a28aef941351b5d72a1c75768a8b65d6c5f
Aman Kumar <amankarguwal0@gmail.com>
2025-05-10 10:04:56 +0530

    Refactor store configuration to include reducers and middleware for auth, misc, chat, and API

--- src/lib/store/store.ts
+++ src/lib/store/store.ts
@@ -1,8 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authSlice from "./auth.reducer";
+import miscSlice from "./misc.reducer";
+import chatSlice from "./chat.reducer";
+import api from "./api";
 
 export const makeStore = () => {
   return configureStore({
-    reducer: {},
+    reducer: {
+      [authSlice.name]: authSlice.reducer,
+      [miscSlice.name]: miscSlice.reducer,
+      [chatSlice.name]: chatSlice.reducer,
+      [api.reducerPath]: api.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(api.middleware),
   });
 };
 
