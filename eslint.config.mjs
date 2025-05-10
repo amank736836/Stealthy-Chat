289ba33a81f2d9a5693715be1c575e87fc61edd0
Aman Kumar <amankarguwal0@gmail.com>
2025-05-10 23:57:48 +0530

    Disable TypeScript rule for requiring imports in ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ const eslintConfig = [
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "@ts-ignore": "off",
+      "@typescript-eslint/no-require-imports": "off",
     },
   }),
 ];
