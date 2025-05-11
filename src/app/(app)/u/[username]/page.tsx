0bddbcec172770cbb2e09ac18cc6c3443032d766
Aman Kumar <amankarguwal0@gmail.com>
2025-05-11 09:50:49 +0530

    Add Navbar component to SendMessage and refactor layout

--- src/app/(app)/u/[username]/page.tsx
+++ src/app/(app)/u/[username]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { messageSchema } from "@/backend/schemas/messageSchema";
+import Navbar from "@/components/Navbar";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import {
@@ -129,106 +130,110 @@ export default function SendMessage() {
   }, [messageString]);
 
   return (
-    <div className="container mx-auto my-8 p-6 bg-white dark:bg-gray-900 rounded-lg max-w-4xl shadow-lg">
-      <h1 className="text-3xl font-bold text-center mb-6 text-gray-900 dark:text-white">
-        Public Profile Link
-      </h1>
+    <>
+      <Navbar />
+      <div className="container mx-auto my-8 p-6 bg-white dark:bg-gray-900 rounded-lg max-w-4xl shadow-lg">
+        <h1 className="text-3xl font-bold text-center mb-6 text-gray-900 dark:text-white">
+          Public Profile Link
+        </h1>
 
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-          <FormField
-            control={form.control}
-            name="content"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel className="text-gray-900 dark:text-gray-300">
-                  Send Anonymous Message to @{username}
-                </FormLabel>
-                <FormControl>
-                  <textarea
-                    {...field}
-                    className="resize-none p-2 w-full border rounded-md dark:bg-gray-700 dark:border-gray-600 dark:text-white"
-                    placeholder="Write your anonymous message here"
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <div className="flex justify-center">
-            <Button
-              type="submit"
-              disabled={isLoading || !messageContent}
-              className="dark:bg-blue-500 dark:text-white"
-            >
-              {isLoading ? (
-                <>
-                  <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                  Please Wait
-                </>
-              ) : (
-                "Send Message"
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              control={form.control}
+              name="content"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-gray-900 dark:text-gray-300">
+                    Send Anonymous Message to @{username}
+                  </FormLabel>
+                  <FormControl>
+                    <textarea
+                      {...field}
+                      className="resize-none p-2 w-full border rounded-md dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+                      placeholder="Write your anonymous message here"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
               )}
-            </Button>
-          </div>
-        </form>
-      </Form>
+            />
 
-      <div className="space-y-6 my-8">
-        <Button
-          onClick={fetchSuggestedMessages}
-          className="w-full dark:bg-gray-700 dark:text-white"
-          disabled={isCompletionLoading}
-        >
-          {isCompletionLoading ? "Loading..." : "Suggest Messages"}
-        </Button>
+            <div className="flex justify-center">
+              <Button
+                type="submit"
+                disabled={isLoading || !messageContent}
+                className="dark:bg-blue-500 dark:text-white"
+              >
+                {isLoading ? (
+                  <>
+                    <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                    Please Wait
+                  </>
+                ) : (
+                  "Send Message"
+                )}
+              </Button>
+            </div>
+          </form>
+        </Form>
 
-        {completionError && (
-          <p className="text-red-500 text-center">{completionError.message}</p>
-        )}
-
-        <Card className="dark:bg-gray-800 dark:border-gray-700">
-          <CardHeader>
-            <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-              Suggested Messages
-            </h3>
-          </CardHeader>
-          <CardContent className="flex flex-col space-y-3">
-            {completionError ? (
-              <p className="text-red-500">{completionError.message}</p>
-            ) : messageString ? (
-              messageArray.map((message, index) => (
-                <Button
-                  key={index}
-                  variant="outline"
-                  onClick={() => handleMessageClick(message)}
-                  className="w-full text-left break-words whitespace-pre-wrap p-3 my-2 border border-gray-300 dark:border-gray-600 rounded-md dark:text-white"
-                >
-                  {message}
-                </Button>
-              ))
-            ) : (
-              <p className="text-gray-500 dark:text-gray-400">
-                No messages to suggest
-              </p>
-            )}
-          </CardContent>
-        </Card>
-      </div>
-
-      <Separator className="my-6 dark:bg-gray-700" />
-
-      <div className="text-center">
-        <p className="mb-4 text-gray-700 dark:text-gray-300">
-          Get Your Own Message Board
-        </p>
-        <Link href="/sign-up">
-          <Button className="dark:bg-blue-500 dark:text-white">
-            Create Your Account
+        <div className="space-y-6 my-8">
+          <Button
+            onClick={fetchSuggestedMessages}
+            className="w-full dark:bg-gray-700 dark:text-white"
+            disabled={isCompletionLoading}
+          >
+            {isCompletionLoading ? "Loading..." : "Suggest Messages"}
           </Button>
-        </Link>
+
+          {completionError && (
+            <p className="text-red-500 text-center">{completionError.message}</p>
+          )}
+
+          <Card className="dark:bg-gray-800 dark:border-gray-700">
+            <CardHeader>
+              <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                Suggested Messages
+              </h3>
+            </CardHeader>
+            <CardContent className="flex flex-col space-y-3">
+              {completionError ? (
+                <p className="text-red-500">{completionError.message}</p>
+              ) : messageString ? (
+                messageArray.map((message, index) => (
+                  <Button
+                    key={index}
+                    variant="outline"
+                    onClick={() => handleMessageClick(message)}
+                    className="w-full text-left break-words whitespace-pre-wrap p-3 my-2 border border-gray-300 dark:border-gray-600 rounded-md dark:text-white"
+                  >
+                    {message}
+                  </Button>
+                ))
+              ) : (
+                <p className="text-gray-500 dark:text-gray-400">
+                  No messages to suggest
+                </p>
+              )}
+            </CardContent>
+          </Card>
+        </div>
+
+        <Separator className="my-6 dark:bg-gray-700" />
+
+        <div className="text-center">
+          <p className="mb-4 text-gray-700 dark:text-gray-300">
+            Get Your Own Message Board
+          </p>
+          <Link href="/sign-up">
+            <Button className="dark:bg-blue-500 dark:text-white">
+              Create Your Account
+            </Button>
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
+
   );
 }
